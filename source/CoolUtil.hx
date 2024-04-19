5bee70e33fbe8446effecfdb2b4738c9015c18bb
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-19 23:23:40 +0800

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -63,13 +63,8 @@ class CoolUtil {
 
 	public static function coolTextFile(path:String):Array<String> {
 		var daList:Array<String> = [];
-		#if sys
-		if (FileSystem.exists(path))
-			daList = File.getContent(path).trim().split('\n');
-		#else
 		if (Assets.exists(path))
 			daList = Assets.getText(path).trim().split('\n');
-		#end
 
 		for (i in 0...daList.length) {
 			daList[i] = daList[i].trim();
