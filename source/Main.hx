9379beedd10026a89da16f16f8123978e88264fb
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-19 18:40:05 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -23,8 +23,8 @@ import neko.vm.Gc;
 
 class Main extends Sprite {
 	public static var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
-	public static var gameWidth:Int = initialState == 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	public static var gameHeight:Int = initialState == 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	public static var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	public static var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
