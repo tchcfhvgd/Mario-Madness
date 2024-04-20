304091fbd098b97bfa4d3e4b11a52358dca04c96
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-20 15:44:07 +0800

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -46,10 +46,10 @@ class ClientPrefs {
 	public static var overworld:Bool = false;
 	public static var deathIHY:Bool = false;
 											//Story Mode[0] | World 1[1] | World 2[2] | World 3[3] | World 4[4] | World 5[5] | Overworld Done[6] | Ultra M Done[7] | Unbeatable Done[8] |Legacy Mode[9] 
-	public static var storySave:Array<Bool> = [false, false, false, false, false, false, false, false, false, false];
+	public static var storySave:Array<Bool> = [true, true, true, true, true, true, true, true, true, true];
 
-	public static var worlds:Array<Int> = [0, 0, 0, 0, 0];
-	public static var worldsALT:Array<Int> = [0, 0, 0, 0, 0]; 
+	public static var worlds:Array<Int> = [3, 7, 5, 6, 3];
+	public static var worldsALT:Array<Int> = [0, 2, 0, 3, 0]; 
 
 	public static var defaultKeys:Array<FlxKey> = [
 		        A,   LEFT, // Note Left
